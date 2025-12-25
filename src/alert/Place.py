13f146e4b731b00224e997e57c59996d7066c756
nuuuwan <nuuuwan@gmail.com>
2025-12-25 08:31:03 +0530

    feat: Add place name to latitude and longitude mapping and update related workflows
    - Introduced a new JSON file `place_name_to_latlng.json` containing mappings of place names to their respective latitude and longitude coordinates.
    - Refactored `Place.py` to improve the handling of city data, ensuring proper logging and data structure.
    - Created a new workflow script `build_from_overpass.py` to build city data from Overpass API responses.
    - Added a legacy workflow `build_place_name_to_latlng.py` to generate a mapping of place names to latitude and longitude from existing place data.
    - Implemented another legacy workflow `build_river_stations.py` to create a new river stations dataset using the newly created place name to latlng mapping.

--- src/alert/Place.py
+++ src/alert/Place.py
@@ -1,4 +1,8 @@
+from utils import Log
+
 from alert.StaticData import StaticData
+
+log = Log("Place")
 
 
 class Place:
@@ -6,27 +10,34 @@ class Place:
     def build_cities():
         places_from_overpass = StaticData("_overpass_cities").read()
 
-        cities = []
-        for elem in places_from_overpass:
+        places = []
+        for overpass_place in places_from_overpass:
 
             try:
-                tags = elem["tags"]
+                tags = overpass_place["tags"]
                 population = int(tags.get("population") or 0)
-                city = dict(
+                place = dict(
                     name=tags.get("name") or tags.get("name:en"),
-                    lat_lng=(round(elem["lat"], 4), round(elem["lon"], 4)),
+                    lat_lng=(
+                        round(overpass_place["lat"], 4),
+                        round(overpass_place["lon"], 4),
+                    ),
                 )
-                if not city["name"]:
-                    log.error(f"Missing name for city with id: {elem['id']}")
+                if not place["name"]:
+                    log.error(
+                        f"Missing name for city with id: {overpass_place['id']}"
+                    )
                     continue
 
                 if population < 10_000:
                     continue
 
-                cities.append(city)
+                places.append(place)
             except KeyError as e:
-                log.error(f"Missing expected tag {e} in element {elem['id']}")
+                log.error(
+                    f"Missing expected tag {e} in element {overpass_place['id']}"
+                )
                 continue
 
-        cities.sort(key=lambda city: city["name"])
-        StaticData("cities").write(cities)
+        places.sort(key=lambda city: city["name"])
+        StaticData("cities").write(places)
